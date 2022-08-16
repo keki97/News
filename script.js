a35c2503f6f5ebe98ccdd40fb9db3529ae0de746
aleksandar kekerovic <aleksandar.kekerovic97@gmail.com>
2022-08-16 09:35:07 +0200

    Solo page style

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
 
 const container = document.querySelector(".container");
 const headerEl = document.querySelector(".header");
+const backToList = document.querySelector(".news-solo-link");
 
 const renderNews = function (articles) {
   articles.forEach(function (el, i) {
@@ -14,7 +15,7 @@ const renderNews = function (articles) {
     <div class='news-card'>
       <h3 class='news-header'>${articles[i].title}</h3>
       <img class='news-img' src='${articles[i].urlToImage}' />
-      <p class='news-content'>${articles[i].description}</p>
+      <div class='content-container'><p class='news-content'>${articles[i].description}</p></div>
       <div class='links'><a class='news-link' href='#' data-id=${i}>More &#x2192</a></div>
     </div>
     `;
@@ -26,13 +27,15 @@ const renderNews = function (articles) {
 
 const renderOneArticle = function (articles) {
   const html = `
-  <div class='news-solo-card'>
-    <h3 class='news-solo-header'>${articles.title}</h3>
-    <div>
-    <img class='news-img' src='${articles.urlToImage}' />
-    <p class='news-content'>${articles.description}</p>
+  <div class='screen-size'>
+    <div class='news-solo-card'>
+      <h3 class='news-solo-header'>${articles.title}</h3>
+      <div class='solo-content-container'>
+      <img class='news-img' src='${articles.urlToImage}' />
+      <p class='news-content'>${articles.content}</p>
+      </div>
+      <a class='news-solo-link' href='#'>&larr; Back to list</a>
     </div>
-    <a class='news-link' href='#'><- Back to list</a>
   </div>
   `;
 
@@ -41,10 +44,8 @@ const renderOneArticle = function (articles) {
 
 container.addEventListener("click", async function (e) {
   e.preventDefault();
-  // console.log(e.target);
   if (e.target.classList.contains("news-link")) {
     const id = e.target.getAttribute("data-id");
-    console.log(id);
 
     container.innerHTML = "";
     headerEl.style.display = "none";
@@ -56,6 +57,11 @@ container.addEventListener("click", async function (e) {
     const articles = data.articles[id];
 
     renderOneArticle(articles);
+  } else if (e.target.classList.contains("news-solo-link")) {
+    container.innerHTML = "";
+    headerEl.style.display = "flex";
+
+    getArticles();
   }
 });
 
@@ -65,7 +71,8 @@ const getArticles = async function () {
   );
   const data = await res.json();
   const articles = data.articles;
-  console.log(articles);
+  const titles = articles.forEach((el) => console.log(el.title));
+  // console.log(articles);
   renderNews(articles);
 };
 
