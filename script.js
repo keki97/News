fc57fe720cae75119d5afb0aea6b6e8a8b701c15
aleksandar kekerovic <aleksandar.kekerovic97@gmail.com>
2022-08-16 13:51:57 +0200

    Add filter

--- script.js
+++ script.js
@@ -7,12 +7,15 @@
 const container = document.querySelector(".container");
 const headerEl = document.querySelector(".header");
 const backToList = document.querySelector(".news-solo-link");
+const searchInput = document.querySelector(".search");
+let articles;
+//  curArticles
 
 const renderNews = function (articles) {
   articles.forEach(function (el, i) {
     if (articles[i].description) {
       const html = `
-    <div class='news-card'>
+    <div id='${i}' class='news-card'>
       <h3 class='news-header'>${articles[i].title}</h3>
       <img class='news-img' src='${articles[i].urlToImage}' />
       <div class='content-container'><p class='news-content'>${articles[i].description}</p></div>
@@ -42,6 +45,21 @@ const renderOneArticle = function (articles) {
   container.insertAdjacentHTML("beforeend", html);
 };
 
+// FIRST PAGE
+const getArticles = async function () {
+  const res = await fetch(
+    "https://newsapi.org/v2/top-headlines?country=us&apiKey=b344ea6570ee4aa98d78614d2ec05a69"
+  );
+  const data = await res.json();
+  articles = data.articles;
+  // const titles = articles.forEach((el) => console.log(el.title));
+  // console.log(articles);
+  renderNews(articles);
+};
+
+getArticles();
+
+// SOLO PAGE //
 container.addEventListener("click", async function (e) {
   e.preventDefault();
   if (e.target.classList.contains("news-link")) {
@@ -65,15 +83,39 @@ container.addEventListener("click", async function (e) {
   }
 });
 
-const getArticles = async function () {
-  const res = await fetch(
-    "https://newsapi.org/v2/top-headlines?country=us&apiKey=b344ea6570ee4aa98d78614d2ec05a69"
-  );
-  const data = await res.json();
-  const articles = data.articles;
-  const titles = articles.forEach((el) => console.log(el.title));
-  // console.log(articles);
-  renderNews(articles);
-};
+// SEARCH FILTER
 
-getArticles();
+function performSearch(target) {
+  let filter, lastFilter;
+
+  filter = target.value;
+  // console.log(filter);
+  if (filter == lastFilter) return;
+  if (!filter) {
+    container.innerHTML = "";
+    renderNews(articles); // when input field cleared, display original articles
+  }
+
+  if (filter.length < 3) return; // only search 3 or more characters in searchTerm
+
+  container.innerHTML = "";
+  renderNews(filterNews(target.value));
+
+  lastFilter = filter;
+}
+
+function filterNews(str) {
+  return articles.filter(
+    (article) =>
+      article.title?.toLowerCase().includes(str.toLowerCase()) ||
+      article.description?.toLowerCase().includes(str.toLowerCase())
+  );
+}
+
+let timeout;
+searchInput.addEventListener("keyup", (e) => {
+  if (timeout) clearTimeout(timeout);
+  timeout = setTimeout(function () {
+    performSearch(e.target);
+  }, 500);
+});
