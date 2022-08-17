a5445c69db997855ae81b2646d78ad9b6705fa3b
aleksandar kekerovic <aleksandar.kekerovic97@gmail.com>
2022-08-17 11:59:21 +0200

    Filter bug fixed

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ const container = document.querySelector(".container");
 const headerEl = document.querySelector(".header");
 const backToList = document.querySelector(".news-solo-link");
 const searchInput = document.querySelector(".search");
-let articles;
-//  curArticles
+let articles, curArticles;
 
 const renderNews = function (articles) {
+  curArticles = articles;
   articles.forEach(function (el, i) {
     if (articles[i].description) {
       const html = `
-    <div id='${i}' class='news-card'>
+    <div class='news-card'>
       <h3 class='news-header'>${articles[i].title}</h3>
       <img class='news-img' src='${articles[i].urlToImage}' />
       <div class='content-container'><p class='news-content'>${articles[i].description}</p></div>
@@ -26,23 +26,6 @@ const renderNews = function (articles) {
       container.insertAdjacentHTML("beforeend", html);
     }
   });
-};
-
-const renderOneArticle = function (articles) {
-  const html = `
-  <div class='screen-size'>
-    <div class='news-solo-card'>
-      <h3 class='news-solo-header'>${articles.title}</h3>
-      <div class='solo-content-container'>
-      <img class='news-img' src='${articles.urlToImage}' />
-      <p class='news-content'>${articles.content}</p>
-      </div>
-      <a class='news-solo-link' href='#'>&larr; Back to list</a>
-    </div>
-  </div>
-  `;
-
-  container.insertAdjacentHTML("beforeend", html);
 };
 
 // FIRST PAGE
@@ -60,8 +43,9 @@ const getArticles = async function () {
 getArticles();
 
 // SOLO PAGE //
-container.addEventListener("click", async function (e) {
+container.addEventListener("click", function (e) {
   e.preventDefault();
+
   if (e.target.classList.contains("news-link")) {
     container.style.display = "block";
     const id = e.target.getAttribute("data-id");
@@ -69,13 +53,19 @@ container.addEventListener("click", async function (e) {
     container.innerHTML = "";
     headerEl.style.display = "none";
 
-    const res = await fetch(
-      "https://newsapi.org/v2/top-headlines?country=us&apiKey=b344ea6570ee4aa98d78614d2ec05a69"
-    );
-    const data = await res.json();
-    const articles = data.articles[id];
-
-    renderOneArticle(articles);
+    const html = `
+    <div class='screen-size'>
+      <div class='news-solo-card'>
+        <h3 class='news-solo-header'>${curArticles[id].title}</h3>
+        <div class='solo-content-container'>
+        <img class='news-img' src='${curArticles[id].urlToImage}' />
+        <p class='news-content'>${curArticles[id].content}</p>
+        </div>
+        <a class='news-solo-link' href='#'>&larr; Back to list</a>
+      </div>
+    </div>
+    `;
+    container.insertAdjacentHTML("beforeend", html);
   } else if (e.target.classList.contains("news-solo-link")) {
     container.innerHTML = "";
     container.style.display = "grid";
@@ -107,11 +97,12 @@ function performSearch(target) {
 }
 
 function filterNews(str) {
-  return articles.filter(
+  const filterArticle = articles.filter(
     (article) =>
       article.title?.toLowerCase().includes(str.toLowerCase()) ||
       article.description?.toLowerCase().includes(str.toLowerCase())
   );
+  return filterArticle;
 }
 
 let timeout;
